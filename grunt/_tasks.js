6e1247d6c4e3014d02d690b95048fb7a55cb1b58
gaohailang <ghlndsl@126.com>
2014-07-21 15:45:51 +0800

    chore(base): remove extract karam(we dont need it, it is clumsy)

--- grunt/_tasks.js
+++ grunt/_tasks.js
@@ -7,13 +7,7 @@ module.exports = function(grunt) {
     ]);
 
     grunt.registerTask('test', [
-        'jshint:test',
-        'karma:test'
-    ]);
-
-    grunt.registerTask('test:travis', [
-        'jshint:test',
-        'karma:travis'
+        'jshint:test'
     ]);
 
     grunt.registerTask('build:staging', [
--- grunt/misc.js
+++ grunt/misc.js
@@ -24,35 +24,6 @@ module.exports = {
         },
         test: ['<%= paths.js %>/**/*.js', '!<%= paths.js %>/vendor/**/*']
     },
-    karma: {
-        options: {
-            configFile: '<%= paths.test %>/karma.conf.js',
-            browsers: ['Chrome_without_security']
-        },
-        server: {
-            reporters: ['progress'],
-            background: true
-        },
-        test: {
-            reporters: ['progress', 'junit', 'coverage'],
-            preprocessors: {
-                '<%= paths.app %>/javascripts/**/*.js': 'coverage'
-            },
-            junitReporter: {
-                outputFile: '<%= paths.test %>/output/test-results.xml'
-            },
-            coverageReporter: {
-                type: 'html',
-                dir: '<%= paths.test %>/output/coverage/'
-            },
-            singleRun: true
-        },
-        travis: {
-            browsers: ['PhantomJS'],
-            reporters: ['progress'],
-            singleRun: true
-        }
-    },
     bump: {
         options: {
             files: ['package.json', 'bower.json'],
