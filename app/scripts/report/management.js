ee73202019e97da1682a37352fb96df016940e7a
gaohailang <ghlndsl@126.com>
2014-10-13 23:21:37 +0800

    feat(management): add multi-kind search for report(support category/group, dimension/metric, report name, owner etc to filter report item etc~~)

--- app/scripts/report/management.js
+++ app/scripts/report/management.js
@@ -1,4 +1,7 @@
 define([], function() {
+    function pluralize(name) {
+        return name.replace(/y$/, 'ie').replace(/$/, 's');
+    }
 
     function managementBaseCtrl($scope, apiHelper) {
         $scope.toggleResStatus = function(enable, type, data) {
@@ -19,12 +22,13 @@ define([], function() {
         return ['$scope', 'apiHelper', '$rootScope', '$modal', '$timeout', function($scope, apiHelper, $rootScope, $modal, $timeout) {
             var capitalizeType = _.capitalize(type);
             var _initFetchData = function() {
-                apiHelper('getDetail' + capitalizeType + 'sList').then(function(data) {
+                apiHelper('getDetail' + capitalizeType + 'sList', {
+                    busy: 'global'
+                }).then(function(data) {
                     $scope[type + 'List'] = data;
                 });
             };
             _initFetchData();
-
 
             $scope['del' + capitalizeType] = function(item) {
                 apiHelper('del' + capitalizeType, item.id).then(function() {
@@ -62,12 +66,10 @@ define([], function() {
             // when a res is added, inject it to list view model
             $rootScope.$on('report:add' + capitalizeType, function(e, data) {
                 _initFetchData();
-                return;
-                $scope[type + 'List'].push(data);
-                // Todo: 滚动到最下面
+                /*$scope[type + 'List'].push(data); // 格式不一样~~
                 $timeout(function() {
-                    $(document).scrollTop(100000);
-                });
+                    $(document).scrollTop(100000); // Todo: 滚动到最下面
+                });*/
             });
             $rootScope.$on('report:edit' + capitalizeType, function(e, data) {
                 _initFetchData();
@@ -79,6 +81,44 @@ define([], function() {
                         return '<tr><td>' + t + '</td><td>' + metric[t] + '</td></tr>';
                     }).join('');
                     return '<table>##</table>'.replace('##', rowsHtml);
+                };
+                $scope.filterOpts = ['report', 'group', 'category', 'metric', 'dimension', 'owner'];
+                $scope.filterField = 'report';
+                $scope.clearFilter = function() {
+
+                };
+                $scope.filterObj = {};
+                $scope.$watch(function() {
+                    return [$scope.filterText, $scope.filterField]
+                }, function() {
+                    $scope.filterObj = {};
+                    // TODO: Metric, dimension, category, report : nested array name
+                    if ($scope.filterText && $scope.filterField) {
+                        if ($scope.filterField === 'report') {
+                            $scope.filterObj.name = $scope.filterText;
+                        } else if ($scope.filterField === 'owner') {
+                            $scope.filterObj[$scope.filterField] = $scope.filterText;
+                        } else {
+                            $scope.filterObj = function(i) {
+                                var _s, passed = false;
+                                _.each(i[pluralize($scope.filterField)], function(i) {
+                                    _s = i.name ? i.name : i;
+                                    console.log(_s);
+                                    if (_s.match(new RegExp($scope.filterText))) {
+                                        passed = true;
+                                    }
+                                });
+                                return passed;
+                            };
+                        }
+                    }
+                }, true);
+                $scope.filterFn = function() {
+                    // cal on each item
+
+                };
+                $scope.applyFilter = function() {
+                    // filter option
                 };
             }
         }]
