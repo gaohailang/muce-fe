05fec26bf0bd7d67ac172dab30cffd5c0fb7a786
gaohailang <ghlndsl@126.com>
2014-10-31 10:52:09 +0800

    chore(mq): update muce query hint keywords list~

--- app/scripts/mq/muce-hint.js
+++ app/scripts/mq/muce-hint.js
@@ -20,7 +20,7 @@
 
     function getKeywords(editor) {
         // by siva
-        var list = 'select,and,between,where,from,if,limit,like'.split(',');
+        var list = 'SELECT,DISTINCT,FROM,WHERE,GROUP,BY,COUNT,AND,p_date,ORDER,DESC,LIMIT,RANK,AS,SORT,DISTRIBUTE,JOIN,COLLECT_SET,ON,IF,IS,NULL,LEFT,OUTER,SEMI,UNION,ALL,IN,RIGHT,SUM,HAVING,LIKE'.split(',');
         var _ret = {};
         _.each(list, function(i) {
             _ret[i] = true;
